67163a831ab3188d6dd7fd002c7a68adde42dec4
David Falk <david.falk@vr-smart-finanz.de>
2019-06-05 15:51:24 +0200

    generated project with spring initializr

---
+++ src/main/java/de/vr/minesweeper/MinesweeperApplication.java
@@ -0,0 +1,14 @@
+package de.vr.minesweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperApplication {
+
+  public static void main(String[] args) {
+    // SpringApplication.run(MinesweeperApplication.class, args);
+    System.out.println("Caaaaaarl, why did you kill all these people?");
+  }
+
+}
