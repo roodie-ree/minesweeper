a7455f415dc511b8e68b8f49495e657387258772
kesslerd <kesslerd@PB000019.vr-gruppe.de>
2019-06-07 15:01:16 +0200

    Merge

--- src/main/java/de/vr/minesweeper/Buttons.java
+++ src/main/java/de/vr/minesweeper/Buttons.java
@@ -1,22 +1,23 @@
 package de.vr.minesweeper;
 
 import java.awt.Component;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Buttons {
-	static JButton button;
+	static JButton button = new JButton(new ImageIcon("src/main/resources/icons8-glücklich-48.png"));;
 	private static ImageIcon shocked=new ImageIcon("src/main/resources/icons8-überrascht-16.png");
 	private static ImageIcon dead=new ImageIcon("src/main/resources/dead-emoticon-smile-icon-isolated-260nw-384153085.jpg");
 	private static ImageIcon smily=new ImageIcon("src/main/resources/icons8-glücklich-48.png");
 	
 	public Component createSmilyButton(){
-		button = new JButton(new ImageIcon("src/main/resources/icons8-glücklich-48.png"));
 	    button.setSize(500,500);
 	    return button;
 	}
+	
 	
 //	public Component createShockedButton(){
 //		JButton button = new JButton(new ImageIcon("src/main/resources/icons8-überrascht-16.png"));
@@ -45,10 +46,5 @@ public class Buttons {
 	
 	
 	
-	public static void main(String[] args)
-    {
-        
-        //boolean selected=false;
-		//button.setSelected(selected);        
-    }
+	
 }
