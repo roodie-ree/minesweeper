8b63523c678c00a85edf9324062e9776545c6635
wieczorekE <wieczorek.esther@gmail.com>
2019-06-07 10:36:30 +0200

    Test commit

--- src/main/java/de/vr/minesweeper/BombCounter.java
+++ src/main/java/de/vr/minesweeper/BombCounter.java
@@ -2,22 +2,8 @@ package de.vr.minesweeper;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.util.Timer;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JToggleButton;
-
 import de.vr.minelogic.MineSweeperTile;
 
 public class BombCounter extends JFrame {
